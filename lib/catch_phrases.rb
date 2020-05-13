28bd7dafdffe8ea50bea32415c5cf276456950a7
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-13 21:09:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,12 @@
-def mario
   phrase = "It's-a me, Mario!"
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
+def mario
+
   puts phrase
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
   puts status
 end
